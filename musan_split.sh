d423841139d65fd8d7b19e2ddefbad010f273d5e
miguel <lamahmichelmarie@gmail.com>
2024-01-08 12:28:52 +0100

    08 Janv 2024 12:28

--- musan_split.sh
+++ musan_split.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=32GB
-# #SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=5
 #SBATCH --output=musan_split_output.log
 #SBATCH --error=musan_split_error.log
 
